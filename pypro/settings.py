d2f58ce79edcf0d63d8ed1e884883853b0071463
Dérick Eduardo <dreduardo2222@gmail.com>
2023-11-27 20:28:19 -0400

    Consertado erro de acesso ao admin do django. close #24

--- pypro/settings.py
+++ pypro/settings.py
@@ -174,3 +174,5 @@ if AWS_ACCESS_KEY_ID:
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
