a1ca94f3da2eabe05b5408f8ba580610405b7a0b
Dérick Eduardo <dreduardo2222@gmail.com>
2023-12-11 11:58:02 -0400

    commit para separar alterações.

--- pypro/settings.py
+++ pypro/settings.py
@@ -154,7 +154,6 @@ if AWS_ACCESS_KEY_ID:
     AWS_AUTO_CREATE_BUCKET = False
     AWS_QUERYSTRING_AUTH = True
     AWS_S3_CUSTOM_DOMAIN = None
-
     COLLECTFAST_STRATEGY = "collectfast.strategies.boto3.Boto3Strategy"
     COLLECTFAST_ENABLED = True
 
