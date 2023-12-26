05fd5b5a572865e2c3ff251b8f26e00873cbcdd4
Dérick Eduardo <dreduardo2222@gmail.com>
2023-12-26 15:38:23 -0400

    Feita migração de dados em modulos com campo slug.

--- pypro/modulos/models.py
+++ pypro/modulos/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from ordered_model.models import OrderedModel
 
 
@@ -6,6 +7,7 @@ class Modulo(OrderedModel):
     titulo = models.CharField(max_length=64)
     publico = models.TextField(null=True)
     descricao = models.TextField(null=True)
+    slug = models.SlugField(unique=True)
     order = models.PositiveIntegerField("order", editable=False, db_index=True, null=True)
 
     class Meta(OrderedModel.Meta):
@@ -13,3 +15,6 @@ class Modulo(OrderedModel):
 
     def __str__(self):
         return self.titulo
+
+    def get_absolute_url(self):
+        return reverse('modulo:detalhe', kwargs={'slug': self.slug})
