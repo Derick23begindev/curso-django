bc309459f00ad7fb4ce176f21c939f207bda27a0
Dérick Eduardo <dreduardo2222@gmail.com>
2023-12-27 13:17:51 -0400

    Relacionamento Muitos para Um.

--- pypro/modulos/admin.py
+++ pypro/modulos/admin.py
@@ -1,10 +1,17 @@
 from django.contrib.admin import register
 from ordered_model.admin import OrderedModelAdmin
 
-from pypro.modulos.models import Modulo
+from pypro.modulos.models import Modulo, Aula
 
 
 @register(Modulo)
 class ModuloAdmin(OrderedModelAdmin):
     list_display = ('titulo', 'publico', 'move_up_down_links')
+    prepopulated_fields = {'slug': ('titulo',)}
+
+
+@register(Aula)
+class AulaAdmin(OrderedModelAdmin):
+    list_display = ('titulo', 'modulo', 'move_up_down_links')
+    list_filter = ('modulo',)
     prepopulated_fields = {'slug': ('titulo',)}
