1399ee9be28c7af173539ba282f634e49681cf2d
Dérick Eduardo <dreduardo2222@gmail.com>
2023-12-30 17:47:53 -0400

    Finalizada funcionalidade de Autenticação e Autorização de usuário. close #73

--- pypro/base/tests/test_login.py
+++ pypro/base/tests/test_login.py
@@ -64,9 +64,9 @@ def test_botao_sair_disponivel(resp_home_com_usuario_logado):
     assert_contains(resp_home_com_usuario_logado, 'Sair')
 
 
-def test_botao_nome_usuario_logado_disponivel(resp_home_com_usuario_logado,usuario_logado):
+def test_botao_nome_usuario_logado_disponivel(resp_home_com_usuario_logado, usuario_logado):
     assert_contains(resp_home_com_usuario_logado, usuario_logado.first_name)
 
 
-def test_link_de_logout_disponivel(resp_home_com_usuario_logado,usuario_logado):
+def test_link_de_logout_disponivel(resp_home_com_usuario_logado, usuario_logado):
     assert_contains(resp_home_com_usuario_logado, reverse('logout'))
